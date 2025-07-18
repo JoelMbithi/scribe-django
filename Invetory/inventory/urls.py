74e1771746d72a03b4feab50c0f1f32628f64611
Joel Mbithi <joellembithi@gmail.com>
2025-07-19 00:11:34 +0300

    done with the dashboard display

--- Invetory/inventory/urls.py
+++ Invetory/inventory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.displayItem,name="base"), 
+    path('home/',views.Dashboard,name="home")
 ]
--- Invetory/inventory/views.py
+++ Invetory/inventory/views.py
@@ -4,6 +4,7 @@ def displayItem(request):
     menu = [
         {
             'label': 'Dashboard',
+            'url': '/home/',
             'icon': '''
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
                      stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -84,3 +85,7 @@ def displayItem(request):
     ]
 
     return render(request, 'Base/base.html', {'menu': menu})
+
+
+def Dashboard(request):
+    return render(request,"Home/home.html")
