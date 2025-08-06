2a8abe5b5b2b420ff5fd8e58c7e72e8cb20c1d48
Joel Mbithi <joellembithi@gmail.com>
2025-08-06 21:23:29 +0300

    still Doing the models part

---
+++ BookingApp/backend/Room_Booking/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    ROOM_TYPES = [
+        ('suite','Suite'),
+        ('standard','Standard Room'),
+        ('deluxe','Deluxe Room')
+    ]
+
+    CURRENCY_TYPES = [
+        ('USD','USD'),
+        ('KSH','KSH'),
+    ]
+
+    name = models.CharField(max_length=200,blank=False)
+    type = models.CharField(max_length=200, choices=ROOM_TYPES)
+    pricePerNight = models.IntegerField(default=150)
+    currency = models.CharField(default="USD",max_length=100,choices=CURRENCY_TYPES)
+    maxOccupancy = models.IntegerField(default=1)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f"{(self.name)} ({self.type})"
+
+
+class RoomImage(models.Model):
+    image = models.ImageField(upload_to="room_images/")
+    caption = models.CharField(max_length=200,blank=True,null=True)
+    room = models.ForeignKey(Room,related_name="images",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Image for {self.room.name} - {self.caption or 'No Caption'}"
+
+
+class OccupiedDate(models.Model):
+    room = models.ForeignKey(Room,on_delete=models.relate_name="occupiedDates")
+    date =  models.DateField()
---
+++ BookingApp/backend/Room_Booking/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from Room_Booking import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('', views.api_root, name="api-root"),
+    path('rooms/', views.RoomList.as_view(), name="room-list"),
+    path('rooms/<int:pk>/',views.RoomDetails.as_view(),name='room-detail')
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
